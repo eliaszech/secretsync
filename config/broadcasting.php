f6859c456b38c07f675c6632013d35b5365fe014
Elias Zech <elias.zech@outlook.com>
2024-01-18 21:13:31 +0100

    Initial Commit

--- config/broadcasting.php
+++ config/broadcasting.php
@@ -29,10 +29,9 @@ return [
     */
 
     'connections' => [
-
         'pusher' => [
             'driver' => 'pusher',
-            'key' => env('PUSHER_APP_KEY'),
+            'auth_key' => env('PUSHER_APP_KEY'),
             'secret' => env('PUSHER_APP_SECRET'),
             'app_id' => env('PUSHER_APP_ID'),
             'options' => [
