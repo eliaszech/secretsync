b8c2765ba25c8f0c18ea1f6ac9c6aeffb55056ff
Elias Zech <elias.zech@outlook.com>
2024-01-18 23:43:21 +0100

    Initial Commit

--- config/broadcasting.php
+++ config/broadcasting.php
@@ -36,8 +36,8 @@ return [
             'app_id' => env('PUSHER_APP_ID', 'dev'),
             'options' => [
                 'cluster' => env('PUSHER_APP_CLUSTER', 'mt1'),
-                'encrypted' => false,
-                'host' => 'forum.secretsync.xyz',
+                'encrypted' => true,
+                'host' => '127.0.0.1',
                 'port' => 6001,
                 'scheme' => 'http',
             ],
