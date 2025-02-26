2e5506dfd4acee38079ad141882d9085227b5bf4
Elias Zech <elias.zech@sitewalk.com>
2025-02-26 15:35:32 +0100

    Set explicit column name for UUID primary key in posts table.
    Updated the migration to define the UUID column with an explicit name ('id') for clarity and consistency. This ensures better readability and avoids potential issues in future schema modifications.

--- database/migrations/2024_01_08_155634_create_posts_table.php
+++ database/migrations/2024_01_08_155634_create_posts_table.php
@@ -12,7 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('posts', function (Blueprint $table) {
-            $table->uuid()->primary();
+            $table->uuid('id')->primary();
             $table->string('title', 200);
             $table->text('description');
             $table->dateTime('deleted')->nullable();
